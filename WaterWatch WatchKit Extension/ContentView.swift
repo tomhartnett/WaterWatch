0508ed1689c67ac41b46fa1ed5c4cbe3451602ac
Tom Hartnett <tomhartnett@gmail.com>
2019-11-21 15:28:57 -0600

    Initial Commit

---
+++ WaterWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterWatch WatchKit Extension
+//
+//  Created by Tom Hartnett on 11/21/19.
+//  Copyright © 2019 Sleekible LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterWatch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WaterWatch WatchKit Extension
+//
+//  Created by Tom Hartnett on 11/21/19.
+//  Copyright © 2019 Sleekible LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
